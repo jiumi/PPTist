3ecca5b4ecf200a6af218c7af9cb984cc7e9190c
玄千 <htl234145@alibaba-inc.com>
2025-08-01 18:58:03 +0800

    add vite build clean

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,16 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import {resolve} from "path";
+import clean from 'vite-plugin-clean'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '',
   plugins: [
     vue(),
+    clean({
+      targetFiles: ['../../webapp/public/pptx']
+    }),
   ],
   server: {
     host: '127.0.0.1',
